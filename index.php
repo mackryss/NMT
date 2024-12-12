aade049edf7a61e85113f1e950c16318d06253f0
ss-ameer <sibuma.syedameer@gmail.com>
2024-12-13 00:28:55 +0800

    folders: init

--- index.php
+++ index.php
@@ -6,11 +6,6 @@
     <title>Index</title>
 </head>
 <body>
-    <h1>hello world</h1>
-    <p>This is a simple website created using HTML and CSS.</p>
-    <ul>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contact.html">Contact</a></li>
-    </ul>
+    
 </body>
 </html>
